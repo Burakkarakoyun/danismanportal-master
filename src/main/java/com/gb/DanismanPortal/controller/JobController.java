fabba09feee18fd6e73ff2c364fe72fbd1c8f3c4
zihniburak <zihniburakkarakoyun@gmail.com>
2021-11-30 02:41:10 +0300

    feature/Controller Job Controller Added.

---
+++ src/main/java/com/gb/DanismanPortal/controller/JobController.java
@@ -0,0 +1,11 @@
+package com.gb.DanismanPortal.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/Job")
+public class JobController {
+}
